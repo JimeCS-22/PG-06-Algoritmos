7486d9ed79f94d246846e3b26723a049af4ff76b
Jimena Calvo <jimenacalvosoto13@gmail.com>
2026-06-01 20:28:10 -0600

    Clase

--- src/test/java/ucr/algoritmos/pg06algoritmos/model/graph/AdjacencyMatrixGraphTest.java
+++ src/test/java/ucr/algoritmos/pg06algoritmos/model/graph/AdjacencyMatrixGraphTest.java
@@ -3,6 +3,8 @@ package ucr.algoritmos.pg06algoritmos.model.graph;
 import org.junit.jupiter.api.Test;
 import ucr.algoritmos.pg06algoritmos.model.linkedList.ListException;
 
+import java.util.Random;
+
 import static org.junit.jupiter.api.Assertions.*;
 class AdjacencyMatrixGraphTest {
 
@@ -16,12 +18,12 @@ class AdjacencyMatrixGraphTest {
 
 
             //agregamos aristas
-            graph.addEdge(1,2);
-            graph.addEdge(1,3);
-            graph.addEdge(2,3);
-            graph.addEdge(2,5);
-            graph.addEdge(3,4);
-            graph.addEdge(4,5);
+            graph.addEdgeAndWeight(1,2, new Random().nextInt(1,30));
+            graph.addEdgeAndWeight(1,3, new Random().nextInt(1,30));
+            graph.addEdgeAndWeight(2,3, new Random().nextInt(1,30));
+            graph.addEdgeAndWeight(2,5, new Random().nextInt(1,30));
+            graph.addEdgeAndWeight(3,4, new Random().nextInt(1,30));
+            graph.addEdgeAndWeight(4,5, new Random().nextInt(1,30));
 
         }catch (ListException e) {
             throw new RuntimeException(e);
