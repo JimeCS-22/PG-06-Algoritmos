61f1e927f321af526631e44e087ac10d826194e8
Jimena Calvo <jimenacalvosoto13@gmail.com>
2026-06-02 09:00:41 -0600

    Metodos restantes

--- src/main/java/ucr/algoritmos/pg06algoritmos/model/graph/AdjacencyMatrixGraph.java
+++ src/main/java/ucr/algoritmos/pg06algoritmos/model/graph/AdjacencyMatrixGraph.java
@@ -1,6 +1,8 @@
 package ucr.algoritmos.pg06algoritmos.model.graph;
 
 import javafx.beans.binding.StringBinding;
+import ucr.algoritmos.pg06algoritmos.model.Queue.ArrayQueue;
+import ucr.algoritmos.pg06algoritmos.model.Queue.LinkedQueue;
 import ucr.algoritmos.pg06algoritmos.model.Queue.MyQueue;
 import ucr.algoritmos.pg06algoritmos.model.Queue.QueueException;
 import ucr.algoritmos.pg06algoritmos.model.linkedList.ListException;
@@ -46,7 +48,7 @@ public class AdjacencyMatrixGraph<T extends Comparable<T>> implements Graph<T> {
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return counter == 0;
     }
 
     @Override
@@ -110,22 +112,115 @@ public class AdjacencyMatrixGraph<T extends Comparable<T>> implements Graph<T> {
 
     @Override
     public void removeVertex(T element) throws GraphException, ListException {
+        if(isEmpty()) throw new GraphException("Adjacency Matrix Graph is Empty");
+
+        int index = indexOf(element);
+        if(index != -1) throw new GraphException("Adjancency Matrix Graph Not Contains Vertex");
+
+        //Se desplaza los vértices hacia la izquierda
+        for (int i = index; i < counter; i++) {
+            vertexList[i] = vertexList[i+1];
+        }
+
+        //Se desplaza las filas en la matriz de adyacencia
+        for (int i = index; i < counter; i++) {
+            for (int j = 0; j < counter; j++) {
+                adjancencyMatrix[i][j] = adjancencyMatrix[i+1][j];
+            }
+        }
+
+        //Desplaza columnas en la matriz de adyacencia
+        for (int i = 0; i < counter; i++) {
+            for (int j = index; j < counter; j++) {
+                adjancencyMatrix[i][j] = adjancencyMatrix[i][j+1];
+            }
+        }
+
+        vertexList[counter - 1] = null; // Limpiar la última posición
+
+        counter--;
 
     }
 
     @Override
     public void removeEdge(T a, T b) throws GraphException, ListException {
+        if (isEmpty()) throw new GraphException("Adjacency Matrix Graph is Empty");
+
+        if (!containsVertex(a) || !containsVertex(b)) throw new GraphException("Adjancency Matrix Graph Not Contains Vertex");
+
+        if (!containsEdge(a,b)) {
+
+            //Grafo dirigido
+            adjancencyMatrix[indexOf(a)][indexOf(b)] = (T) Integer.valueOf(0);
+            // Grafo no dirigido
+            adjancencyMatrix[indexOf(b)][indexOf(a)] = (T) Integer.valueOf(0);
+        }
 
     }
 
     @Override
     public String dfs() throws GraphException, StackException, ListException {
-        return "";
+        if(isEmpty())
+            throw new GraphException("Adjacency Matrix Graph is Empty");
+
+        StringBuilder sb = new StringBuilder();
+        boolean[] visited = new boolean[counter];
+
+        for(int i = 0; i < counter; i++) {
+            if(!visited[i]) {
+                dfsProceso(i, visited, sb);
+            }
+        }
+
+        return sb.toString().trim();
     }
 
+    private void dfsProceso(int index, boolean[] visited, StringBuilder sb) {
+        visited[index] = true;
+        sb.append(vertexList[index].data).append(" ");
+
+        for(int j = 0; j < counter; j++) {
+            if(!visited[j] && adjancencyMatrix[index][j] != null
+                    && !adjancencyMatrix[index][j].equals((T)Integer.valueOf(0))) {
+                dfsProceso(j, visited, sb);
+            }
+        }
+    }
+
+
     @Override
-    public String bfs() throws GraphException, QueueException, ListException {
-        return "";
+    public String bfs() throws GraphException, ListException {
+        if(isEmpty())
+            throw new GraphException("Adjacency Matrix Graph is Empty");
+
+        StringBuilder sb = new StringBuilder();
+        boolean[] visited = new boolean[counter];
+        int[] queue = new int[counter];
+        int front = 0, rear = 0;
+
+        for(int i = 0; i < counter; i++) {
+            if(!visited[i]) {
+                queue[rear++] = i;
+                visited[i] = true;
+
+                while(front < rear) {
+                    int current = queue[front++];
+                    sb.append(vertexList[current].data).append(" ");
+
+                    for(int j = 0; j < counter; j++) {
+                        if(!visited[j] && adjancencyMatrix[current][j] != null
+                                && !adjancencyMatrix[current][j].equals((T)Integer.valueOf(0))) {
+                            queue[rear++] = j;
+                            visited[j] = true;
+                        }
+                    }
+                }
+                front = 0;
+                rear = 0;
+            }
+        }
+
+        return sb.toString().trim();
     }
 
     @Override
--- src/test/java/ucr/algoritmos/pg06algoritmos/model/graph/AdjacencyMatrixGraphTest.java
+++ src/test/java/ucr/algoritmos/pg06algoritmos/model/graph/AdjacencyMatrixGraphTest.java
@@ -1,13 +1,15 @@
 package ucr.algoritmos.pg06algoritmos.model.graph;
 
 import org.junit.jupiter.api.Test;
+import ucr.algoritmos.pg06algoritmos.model.Queue.QueueException;
 import ucr.algoritmos.pg06algoritmos.model.linkedList.ListException;
+import ucr.algoritmos.pg06algoritmos.model.stack.StackException;
 
 import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.*;
-class AdjacencyMatrixGraphTest {
 
+class AdjacencyMatrixGraphTest {
 
     @Test
     public void testAdjacencyMatrixGraph() {
@@ -16,18 +18,66 @@ class AdjacencyMatrixGraphTest {
             for (int i = 0; i <= 5; i++)
                 graph.addVertex(i);
 
+            //agregamos aristas con pesos
+            graph.addEdgeAndWeight(1, 2, new Random().nextInt(1, 30));
+            graph.addEdgeAndWeight(1, 3, new Random().nextInt(1, 30));
+            graph.addEdgeAndWeight(2, 3, new Random().nextInt(1, 30));
+            graph.addEdgeAndWeight(2, 5, new Random().nextInt(1, 30));
+            graph.addEdgeAndWeight(3, 4, new Random().nextInt(1, 30));
+            graph.addEdgeAndWeight(4, 5, new Random().nextInt(1, 30));
 
-            //agregamos aristas
-            graph.addEdgeAndWeight(1,2, new Random().nextInt(1,30));
-            graph.addEdgeAndWeight(1,3, new Random().nextInt(1,30));
-            graph.addEdgeAndWeight(2,3, new Random().nextInt(1,30));
-            graph.addEdgeAndWeight(2,5, new Random().nextInt(1,30));
-            graph.addEdgeAndWeight(3,4, new Random().nextInt(1,30));
-            graph.addEdgeAndWeight(4,5, new Random().nextInt(1,30));
 
-        }catch (ListException e) {
+            System.out.println(graph);
+
+            // Prueba de DFS
+            System.out.println("\n=== PRUEBA DFS ===");
+            System.out.println("Recorrido DFS: " + graph.dfs());
+
+            // Prueba de BFS
+            System.out.println("\n=== PRUEBA BFS ===");
+            System.out.println("Recorrido BFS: " + graph.bfs());
+
+            // Prueba de containsVertex
+            System.out.println("\n=== PRUEBA containsVertex ===");
+            System.out.println("¿Contiene vértice 3? " + graph.containsVertex(3));
+            System.out.println("¿Contiene vértice 10? " + graph.containsVertex(10));
+
+            // Prueba de containsEdge
+            System.out.println("\n=== PRUEBA containsEdge ===");
+            System.out.println("¿Existe arista entre 1 y 2? " + graph.containsEdge(1, 2));
+            System.out.println("¿Existe arista entre 1 y 5? " + graph.containsEdge(1, 5));
+
+            // Prueba de size
+            System.out.println("\n=== PRUEBA size ===");
+            System.out.println("Tamaño del grafo: " + graph.size());
+
+            // Prueba de removeEdge
+            System.out.println("\n=== PRUEBA removeEdge ===");
+            System.out.println("Eliminando arista entre 2 y 5...");
+            graph.removeEdge(2, 5);
+            System.out.println("¿Existe arista entre 2 y 5? " + graph.containsEdge(2, 5));
+            System.out.println("\nGrafo después de eliminar arista:");
+            System.out.println(graph);
+
+            // Prueba de removeVertex
+            System.out.println("\n=== PRUEBA removeVertex ===");
+            System.out.println("Eliminando vértice 3...");
+            graph.removeVertex(3);
+            System.out.println("Tamaño del grafo después de eliminar: " + graph.size());
+            System.out.println("\nGrafo después de eliminar vértice:");
+            System.out.println(graph);
+
+            // Prueba de DFS después de cambios
+            System.out.println("\n=== PRUEBA DFS (después de cambios) ===");
+            System.out.println("Recorrido DFS: " + graph.dfs());
+
+            // Prueba de BFS después de cambios
+            System.out.println("\n=== PRUEBA BFS (después de cambios) ===");
+            System.out.println("Recorrido BFS: " + graph.bfs());
+
+        } catch (ListException | StackException e) {
             throw new RuntimeException(e);
         }
-        System.out.println(graph);
     }
+
 }
