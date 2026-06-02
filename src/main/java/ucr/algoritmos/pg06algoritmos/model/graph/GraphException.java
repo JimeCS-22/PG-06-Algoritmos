7c291a616d162b9492b4d6aaf8f06fea05ef9df8
Jimena Calvo <jimenacalvosoto13@gmail.com>
2026-06-01 18:46:39 -0600

    Clase

---
+++ src/main/java/ucr/algoritmos/pg06algoritmos/model/graph/GraphException.java
@@ -0,0 +1,7 @@
+package ucr.algoritmos.pg06algoritmos.model.graph;
+
+public class GraphException extends RuntimeException {
+    public GraphException(String message) {
+        super(message);
+    }
+}
