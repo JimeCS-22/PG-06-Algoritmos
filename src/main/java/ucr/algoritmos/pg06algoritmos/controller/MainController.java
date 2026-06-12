3f01e5e9b61b74a002a9913d18c2f1aaf9629741
Jimena Calvo <jimenacalvosoto13@gmail.com>
2026-06-11 20:35:49 -0600

    Parte grafica Matriz

--- src/main/java/ucr/algoritmos/pg06algoritmos/controller/MainController.java
+++ src/main/java/ucr/algoritmos/pg06algoritmos/controller/MainController.java
@@ -1,4 +1,107 @@
 package ucr.algoritmos.pg06algoritmos.controller;
 
+import javafx.scene.canvas.Canvas;
+import javafx.scene.control.*;
+
 public class MainController {
+    @javafx.fxml.FXML
+    private Button btnEliminarInicio;
+    @javafx.fxml.FXML
+    private TextField txtName;
+    @javafx.fxml.FXML
+    private Button btnClearListDoble;
+    @javafx.fxml.FXML
+    private TextArea txAreaNodeStructureD;
+    @javafx.fxml.FXML
+    private Button generateGrafo;
+    @javafx.fxml.FXML
+    private Button btnSearchAvl;
+    @javafx.fxml.FXML
+    private TableColumn colStock;
+    @javafx.fxml.FXML
+    private Label txtInsertar;
+    @javafx.fxml.FXML
+    private ComboBox numVertex;
+    @javafx.fxml.FXML
+    private Label txtInsertadoInDoubly;
+    @javafx.fxml.FXML
+    private Label lblAvlInfo;
+    @javafx.fxml.FXML
+    private Canvas canvasMatriz;
+    @javafx.fxml.FXML
+    private TextField infoAristas;
+    @javafx.fxml.FXML
+    private Button btnAgregarDoble;
+    @javafx.fxml.FXML
+    private Button btnOrdenarStock;
+    @javafx.fxml.FXML
+    private Button btnRemoveAvl;
+    @javafx.fxml.FXML
+    private TabPane mainTabs;
+    @javafx.fxml.FXML
+    private TextField txtStock;
+    @javafx.fxml.FXML
+    private TableColumn colIdDoubly;
+    @javafx.fxml.FXML
+    private ListView listViewOperationsListDoubly;
+    @javafx.fxml.FXML
+    private Button btnPlayAvl;
+    @javafx.fxml.FXML
+    private CheckBox checkGrafo;
+    @javafx.fxml.FXML
+    private Button btnAddAvl;
+    @javafx.fxml.FXML
+    private TextField txtId;
+    @javafx.fxml.FXML
+    private Canvas canvasGrafoMatriz;
+    @javafx.fxml.FXML
+    private TableColumn colRegDate;
+    @javafx.fxml.FXML
+    private TextField txtPrice;
+    @javafx.fxml.FXML
+    private Button btnOrdenarNombre;
+    @javafx.fxml.FXML
+    private Button btnEliminarDoble;
+    @javafx.fxml.FXML
+    private Canvas canvasAVL;
+    @javafx.fxml.FXML
+    private Label lblAvlInfo1;
+    @javafx.fxml.FXML
+    private TableView tableCircularDoubly;
+    @javafx.fxml.FXML
+    private Label lblTours;
+    @javafx.fxml.FXML
+    private TableColumn colNombre;
+    @javafx.fxml.FXML
+    private Button Clean;
+    @javafx.fxml.FXML
+    private Label spaceInfo;
+    @javafx.fxml.FXML
+    private ChoiceBox bxType;
+    @javafx.fxml.FXML
+    private Button btnSearchDoble;
+    @javafx.fxml.FXML
+    private Button loadExamples;
+    @javafx.fxml.FXML
+    private Button btnEliminarFinal;
+    @javafx.fxml.FXML
+    private TableColumn colType;
+    @javafx.fxml.FXML
+    private TextArea txAreaNodeRepreDoubly;
+    @javafx.fxml.FXML
+    private Canvas canvasListDoubly;
+    @javafx.fxml.FXML
+    private Label lblRotacion;
+    @javafx.fxml.FXML
+    private ComboBox cbRecorridos;
+    @javafx.fxml.FXML
+    private ListView listViewInfoMatriz;
+    @javafx.fxml.FXML
+    private DatePicker dpRegisterDate;
+    @javafx.fxml.FXML
+    private Button btnClearAvl;
+    @javafx.fxml.FXML
+    private TableColumn colPrice;
+    @javafx.fxml.FXML
+    private TextField txtAvlValue;
 }
